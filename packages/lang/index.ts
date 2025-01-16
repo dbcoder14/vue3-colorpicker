d6bd2ed216a1e0d6ffe36a0a0fcec6404e595952
dbCoder <96175955+dbcoder14@users.noreply.github.com>
2025-01-16 14:01:39 +0100

    Update index.ts

--- packages/lang/index.ts
+++ packages/lang/index.ts
@@ -1,9 +1,11 @@
 import enUS from "./en-US";
 import zhCn from "./zh-CN";
+import daDK from "./da-DK";
 
-export type Lang = "ZH-cn" | "En";
+export type Lang = "ZH-cn" | "En" | "DK-da";
 
 export const Local: { [K in Lang]: { [key: string]: string } } = {
   En: enUS,
   "ZH-cn": zhCn,
+  "DK-da": daDK,
 };
